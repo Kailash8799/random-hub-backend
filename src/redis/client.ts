00adc6fa1757113667921ff9c638192a425908fa
Kailash8799 <kailashrajput8799@gmail.com>
2024-03-07 09:05:16 +0530

    change redis host and port

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -1,5 +1,11 @@
 import { Redis } from 'ioredis';
 
-const redisClient = new Redis({});
+const redisClient = new Redis({
+    port: 6379,
+    host: process.env.RANDOMHUB_BACKEND,
+    username: "default", // needs Redis >= 6
+    password: "my-top-secret",
+    db: 0,
+});
 
 export { redisClient };
